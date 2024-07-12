45b75378e11f06b08068b86e8432fdcff8b4343c
iamrealwilson <wilsonmarasigan@aol.com>
2024-07-12 10:51:44 +0800

    ...

---
+++ src/questions.js
@@ -0,0 +1,89 @@
+const questions = [
+  {
+    "id": 1,
+    "text": "The dictatorial regime of President A of the Republic of Gordon was toppled by a combined force led by Gen. Abe, former royal guards and the secessionist Gordon People’s Army. The new government constituted a Truth and Reconciliation Commission to look into the serious crimes committed under President A’s regime. After the hearings, the Commission recommended that an amnesty law be passed to cover even those involved in mass killings of members of indigenous groups who opposed President A. International human rights groups argued that the proposed amnesty law is contrary to international law. Decide with reasons. (4%)",
+    "type": "essay"
+  },
+  {
+    "id": 2,
+    "text": "Compare and contrast the jurisdiction of the International Criminal Court and International Court of Justice. (3%)",
+    type: "essay"
+  },
+  {
+    "id": 3,
+    "text": "A, a British photojournalist, was covering the violent protests of the Thai Red-Shirts Movement in Bangkok. Despite warnings given by the Thai Prime Minister to foreigners, specially journalists, A moved around the Thai capital. In the course of his coverage, he was killed with a stray bullet which was later identified as having come from the ranks of the Red-Shirts. The wife of A sought relief from Thai authorities but was refused assistance. Is there state responsibility on the part of Thailand? (2%) What is the appropriate remedy available to the victim’s family under international law? (3%)",
+    "type": "essay"
+  },
+  {
+    "id": 4,
+    "text": "Choose the statement which appropriately completes the opening phrase: 'A State which resorts to retorsion in international law (a) must ensure that all states consent to its act. (b) cannot curtail migration from the offending state. (c) Can expel the nationals of the offending state. (d) should apply proportionate response within appreciable limit. (e) None of the above. Explain your answer. (2%)",
+    type: "essay"
+  },
+  {
+    "id": 5,
+    "text": "Congresswoman A is a co-owner of an industrial estate in Sta. Rosa, Laguna which she had declared in her Statement of Assets and Liabilities. A member of her political party authored a bill which would provide a 5-year development plan for all industrial estates in the Southern Tagalog Region to attract investors. The plan included an appropriation of 2 billion pesos for construction of roads around the estates. When the bill finally became law, a civil society watchdog questioned the constitutionality of the law as it obviously benefitted Congresswoman A’s industrial estate. Decide with reasons. (3%)",
+    type: "essay"
+  },
+  {
+    "id": 6,
+    "text": "The “Poverty Alleviation and Assistance Act” was passed to enhance the capacity of the most marginalized families nationwide. A financial assistance scheme called “conditional cash transfers” was initially funded 500 million pesos by Congress. One of the provisions of the law gave the Joint-Congressional Oversight Committee authority to screen the list of beneficiary families initially determined by the Secretary of Department of Social Welfare and Development pursuant to the Department implementing rules. Mang Pandoy, a resident of Smokey Mountain in Tondo, questioned the authority of the Committee. Does Mang Pandoy have legal standing to question the law?(2%) Is the grant of authority to the Oversight Committee to screen beneficiaries constitutional? (3%) Decide with reasons.",
+    type: "essay"
+  },
+  {
+    "id": 7,
+    "text": "True or False. A proclamation of a state of emergency is sufficient to allow the President to take over any public utility. (0.5%) A treaty which provides tax exemption needs no concurrence by a majority of all the Members of the Congress. (0.5%)",
+    type: "essay"
+  },
+  {
+    "id": 8,
+    "text": "Distinguish `presidential communications privilege` from `deliberative process privilege.` (3%)",
+    type: "essay"
+  },
+  {
+    "id": 9,
+    "text": "The League of Filipino Political Scientists (LFPS) organized an international conference on the human rights situation in Myanmar at the Central Luzon State University (CLSU). An exiled Myanmar professor Sung Kui, critical of the military government in Myanmar, was invited as keynote speaker. The Secretary of Foreign Affairs informed the President of the regional and national security implications of having Prof. Kui address the conference. The President thereupon instructed the immigration authorities to prevent the entry of Prof. Kui into Philippine territory. The chancellor of CLSU argued that the instruction violates the Constitution. Decide with reasons. (4%)",
+    type: "essay"
+  },
+  {
+    "id": 10,
+    "text": "A, the wife of an alleged victim of enforced disappearance, applied for the issuance of a Writ of Amparo before a Regional Trial Court in Tarlac. Upon motion of A, the court issued inspection and production orders addressed to the AFP Chief of Staff to allow entry at Camp Aquino and permit the copying of relevant documents, including the list of detainees, if any. Accompanied by court-designated Commission on Human Rights (CHR) lawyers, A took photographs of a suspected isolation cell where her husband was allegedly seen being held for three days and tortured before he finally disappeared. The CHR lawyers requested one Lt. Valdez for a photocopy of the master plan of Camp Aquino and to confirm in writing that he had custody of the master plan. Lt. Valdez objected on the ground that it may violate his right against self-incrimination. Decide with reasons. (4%)",
+    type: "essay"
+  },
+  {
+    "id": 11,
+    "text": "A witnessed two hooded men with baseball bats enter the house of their next door neighbor B. After a few seconds, he heard B shouting, “Huwag Pilo babayaran kita agad.” Then A saw the two hooded men hitting B until the latter fell lifeless. The assailants escaped using a yellow motorcycle with a fireball sticker on it toward the direction of an exclusive village nearby. A reported the incident to PO1 Nuval. The following day, PO1 Nuval saw the motorcycle parked in the garage of a house at Sta. Ines Street inside the exclusive village. He inquired with the caretaker as to who owned the motorcycle. The caretaker named the brothers Pilo and Ramon Maradona who were then outside the country. PO1 Nuval insisted on getting inside the garage. Out of fear, the caretaker allowed him. PO1 Nuval took 2 ski masks and 2 bats beside the motorcycle. Was the search valid? What about the seizure? Decide with reasons. (4%)",
+    type: "essay"
+  },
+  {
+    "id": 12,
+    "text": "ABC operates an industrial waste processing plant within Laoag City. Occasionally, whenever fluid substances are released through a nearby creek, obnoxious odor is emitted causing dizziness among residents in Barangay La Paz. On complaint of the Punong Barangay, the City Mayor wrote ABC demanding that it abate the nuisance. This was ignored. An invitation to attend a hearing called by the Sangguniang Panlungsod was also declined by the president of ABC. The city government thereupon issued a cease and desist order to stop the operations of the plant, prompting ABC to file a petition for injunction before the Regional Trial Court, arguing that the city government did not have any power to abate the alleged nuisance. Decide with reasons. (3%)",
+    type: "essay"
+  },
+  {
+    "id": 13,
+    "text": "Rudy Domingo, 38 years old, natural-born Filipino and a resident of the Philippines since birth, is a Manila-based entrepreneur who runs KABAKA, a coalition of peoples’ organizations from fisherfolk communities. KABAKA’s operations consist of empowering fisherfolk leaders through livelihood projects and trainings on good governance. The Dutch Foundation for Global Initiatives, a private organization registered in The Netherlands, receives a huge subsidy from the Dutch Foreign Ministry, which, in turn is allocated worldwide to the Foundation’s partners like KABAKA. Rudy seeks to register KABAKA as a party-list with himself as a nominee of the coalition. Will KABAKA and Rudy be qualified as a party-list and a nominee, respectively? Decide with reasons. (4%)",
+    type: "essay"
+  },
+  {
+    "id": 14,
+    "text": "During his campaign sortie in Barangay Salamanca, Mayor Galicia was arrested at a PNP checkpoint for carrying highpowered firearms in his car. He was charged and convicted for violation of the COMELEC gun ban. He did not appeal his conviction and instead applied for executive clemency. Acting on the favorable recommendation of the Board of Pardons and Parole, the President granted him pardon. Is he eligible to run again for an elective position? Explain briefly. (5%)",
+    type: "essay"
+  },
+  {
+    "id": 15,
+    "text": "The House Committee on Appropriations conducted an inquiry in aid of legislation into alleged irregular and anomalous disbursements of the Countrywide Development Fund (CDF) and Congressional Initiative Allocation (CIA) of Congressmen as exposed by X, a Division Chief of the Department of Budget and Management (DBM). Implicated in the questionable disbursements are high officials of the Palace. The House Committee summoned X and the DBM Secretary to appear and testify. X refused to appear, while the Secretary appeared but refused to testify invoking executive privilege. May X be compelled to appear and testify? If yes, what sanction may be imposed on him? (2%) Is the Budget Secretary shielded by executive privilege from responding to the inquiries of the House Committee? Explain briefly. If the answer is no, is there any sanction that may be imposed upon him? (3%)",
+    type: "essay"
+  },
+  {
+    "id": 16,
+    "text": "To instill religious awareness in the students of Doña Trinidad High School, a public school in Bulacan, the Parent- Teacher’s Association of the school contributed funds for the construction of a grotto and a chapel where ecumenical religious services and seminars are being held after school hours. The use of the school grounds for these purposes was questioned by a parent who does not belong to any religious group. As his complaint was not addressed by the school officials, he filed an administrative complaint against the principal before the DECS. Is the principal liable? Explain briefly. (5%)",
+    type: "essay"
+  },
+  {
+    "id": 17,
+    "text": "Compare and contrast “overbreadth doctrine” from “void-forvagueness” doctrine. (5%)",
+    type: "essay"
+  }
+  ];
+
+export default questions;
